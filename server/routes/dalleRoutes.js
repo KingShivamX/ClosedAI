f126b7d1e09a7fe59206bfdd1f2d83019f35c602
KingShivamX <shivamhippalgave@gmail.com>
2024-09-21 02:41:16 +0530

    yes

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,37 +1,41 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import OpenAI from 'openai';
+import express from "express"
+import dotenv from "dotenv"
+import OpenAI from "openai"
 
-dotenv.config();
+dotenv.config()
 
-const router = express.Router();
+const router = express.Router()
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
-});
-
+})
 
 router.route("/").get((req, res) => {
     res.send("hello from Dalleee")
 })
 
 router.route("/").post(async (req, res) => {
-    try{
-        const { prompt } = req.body;
-        const response = await openai.images.generate({
+    try {
+        const { prompt } = req.body
+        const aiResponse = await openai.images.generate({
             prompt,
             n: 1,
             size: "1024x1024",
-            response_format: 'b64_json'
-        });
-
-        const image = aiResponse.data.data[0].b64_json;
-        res.status(200).json({photo: image});
-
+            response_format: "b64_json",
+        })
+        if (aiResponse.data && aiResponse.data.length > 0) {
+            const image = aiResponse.data[0].b64_json
+            res.status(200).json({ photo: image })
+        } else {
+            res.status(500).json({ error: "Image generation failed." })
+        }
     } catch (err) {
-        console.log("There is a error shivam", err)
-        res.status(500).send(err?.response.data.error.message);
+        console.log("There is an error, Shivam", err)
+        res.status(500).send(
+            err?.response?.data?.error?.message ||
+                "An error occurred while generating the image."
+        )
     }
 })
 
-export default router;
+export default router
