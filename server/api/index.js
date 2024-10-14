18d6b3cd99e12da48ae6bf915885ad8b8ea9ddff
KingShivamX <shivamhippalgave@gmail.com>
2024-10-14 06:34:26 +0530

    CORS

--- server/api/index.js
+++ server/api/index.js
@@ -9,7 +9,13 @@ import dalleRoutes from "../routes/dalleRoutes.js"
 dotenv.config()
 
 const app = express()
-app.use(cors())
+// Allow CORS from your frontend
+const corsOptions = {
+    origin: "https://closed-ai-gen.vercel.app",
+    optionsSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
 app.use(express.json({ limit: "50mb" }))
 
 app.use("/api/v1/post", postRoutes)
